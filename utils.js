a34aeb0aff14165400068227928dbbc7e2850f5d
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-01-26 16:09:48 -0800

    fixed typo

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 export function makeStatsString(headNum, middleNum, bottomNum) {
-    return ` ${headNum} ${middleNum} ${bottomNum}`;
+    return ` You picked your head ${headNum} You picked your middle ${middleNum} You picked your bottom ${bottomNum}`;
 }
